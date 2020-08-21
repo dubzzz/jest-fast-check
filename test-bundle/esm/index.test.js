4b7f7086ea1ea7ef3ed313f4bac1a896a6071ae9
Nicolas DUBIEN <github@dubien.org>
2020-08-21 09:22:51 +0200

    Hybrid bundle for both commonjs and esm

---
+++ test-bundle/esm/index.test.js
@@ -0,0 +1,11 @@
+import { testProp, fc } from "jest-fast-check";
+
+// for all a, b, c strings
+// b is a substring of a + b + c
+testProp(
+  "should detect the substring",
+  [fc.string(), fc.string(), fc.string()],
+  (t, a, b, c) => {
+    t.true((a + b + c).includes(b));
+  }
+);
