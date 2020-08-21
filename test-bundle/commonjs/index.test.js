9e183fa1b041b6335adf23774460f94adf9ff555
Nicolas DUBIEN <github@dubien.org>
2020-08-21 09:28:36 +0200

    Bad copy-paste

--- test-bundle/commonjs/index.test.js
+++ test-bundle/commonjs/index.test.js
@@ -1,7 +1,11 @@
-const { testProp, fc } = require('ava-fast-check');
+const { testProp, fc } = require("jest-fast-check");
 
 // for all a, b, c strings
 // b is a substring of a + b + c
-testProp('should detect the substring', [fc.string(), fc.string(), fc.string()], (t, a, b, c) => {
-  t.true((a + b + c).includes(b));
-});
+testProp(
+  "should detect the substring",
+  [fc.string(), fc.string(), fc.string()],
+  (t, a, b, c) => {
+    t.true((a + b + c).includes(b));
+  }
+);
