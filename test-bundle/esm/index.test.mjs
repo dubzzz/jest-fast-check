6ed24a0a1837da0d13de93b8bdf83162882a3e02
Nicolas DUBIEN <github@dubien.org>
2020-08-21 19:45:07 +0200

    Trying stuff

--- test-bundle/esm/index.test.mjs
+++ test-bundle/esm/index.test.mjs
@@ -3,8 +3,9 @@
 // Jest is currently adding support ES Modules based on this flag
 // More on: https://github.com/facebook/jest/issues/9430
 
-import { testProp, fc } from "jest-fast-check";
+import { testProp } from "jest-fast-check";
 
+const fc = {};
 // for all a, b, c strings
 // b is a substring of a + b + c
 testProp(
